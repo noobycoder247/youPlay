718f297cb017fa9b7f5f8108cfadd967a50639b1
sandip mahato <sandip.sizemetric@outlook.com>
2024-05-15 14:29:00 +0530

    connected to db

---
+++ src/db/index.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        console.log("---", `${process.env.MONGODB_URI}/${DB_NAME}`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(`Mogodb Conected!! DB HOST: ${connectionInstance} ${connectionInstance.connection.host}`);
+    } catch(error) {
+        console.error("Mongo DB Connection Error", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
